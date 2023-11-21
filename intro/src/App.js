af805dc2f72d0059b8d8a4a6e150e4ade714c3df
Takowski <93830775+Takowski@users.noreply.github.com>
2023-11-21 10:37:14 +0100

    tailwind init

--- intro/src/App.js
+++ intro/src/App.js
@@ -17,8 +17,12 @@ function App() {
         >
           Learn React @ BeCode
         </a>
+        <h1 className="text-3xl text-black font-bold underline">
+      Hello world!
+    </h1>
       </header>
     </div>
+    
   );
 }
 
